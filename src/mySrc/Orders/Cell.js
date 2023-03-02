9e1b4e99751bf6620d51f7ef20bb1fa57489a605
meomeok9 <hoannmfx17428@funix.edu.vn>
2023-03-02 12:39:10 +0700

    fix img load form BE

--- src/mySrc/Orders/Cell.js
+++ src/mySrc/Orders/Cell.js
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
+import useFixUrl from "../hooks/useFixUrl";
 import "./Cell.css";
 
 const Cell = (props) => {
+  const { fix } = useFixUrl();
   const isHead = props.isHead;
   const button = props.button;
   const img = props.img;
@@ -24,10 +26,26 @@ const Cell = (props) => {
       )}
       {img && src && (
         <Fragment>
-          <img style={{ width: '24%' }} src={props.src[0]} alt={props.title} />
-          <img style={{ width: '24%' }} src={props.src[1]} alt={props.title} />
-          <img style={{ width: '24%' }} src={props.src[2]} alt={props.title} />
-          <img style={{ width: '24%' }} src={props.src[3]} alt={props.title} />
+          <img
+            style={{ width: "24%" }}
+            src={fix(props.src[0])}
+            alt={props.title}
+          />
+          <img
+            style={{ width: "24%" }}
+            src={fix(props.src[1])}
+            alt={props.title}
+          />
+          <img
+            style={{ width: "24%" }}
+            src={fix(props.src[2])}
+            alt={props.title}
+          />
+          <img
+            style={{ width: "24%" }}
+            src={fix(props.src[3])}
+            alt={props.title}
+          />
         </Fragment>
       )}
     </div>
